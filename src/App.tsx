9f55cd61d44a30bfbe8e8bdc6e8ac8e31cb0c6b5
Gabriel Schu <schu-gabriel@hotmail.com>
2025-03-10 22:05:22 -0300

    :art: Adicionando global css para estilização

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import { useState } from "react";
+import { ThemeProvider } from "styled-components";
+
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <p>Hello World</p>
-    </>
+      <GlobalStyle />
+    </ThemeProvider>
   );
 }
